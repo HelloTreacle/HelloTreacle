a79a02dfb192eda521dc0cf9f08e56384319ad0d
Alex Brown <alex@cohoda.com>
2017-06-30 23:34:55 +0100

    added tests for property retriever / headers extraction

---
+++ HelloTreacle.Tests/Requests/Properties/Retrieval/DefaultRequestPropertyRetrieverTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using HelloTreacle.Requests.Properties.Retrieval;
+using Microsoft.Owin;
+using NUnit.Framework;
+
+namespace HelloTreacle.Tests.Requests.Properties.Retrieval
+{
+    [TestFixture]
+    public class DefaultRequestPropertyRetrieverTests
+    {
+        private DefaultRequestPropertyRetriever _defaultRequestPropertyRetriever;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _defaultRequestPropertyRetriever = new DefaultRequestPropertyRetriever();
+        }
+
+        [Test]
+        public void Retrieves_keys_and_values_of_headers()
+        {
+            var request = new OwinRequest();
+            request.Headers.Add("header1", new[] { "value1" });
+            request.Headers.Add("header2", new[] { "value2" });
+            request.Headers.Add("header3", new[] { "value1", "value2" });
+
+            var retreivedProperties = _defaultRequestPropertyRetriever.Retrieve(request);
+
+            Assert.That(retreivedProperties.ElementAt(0).Key, Is.EqualTo("header1"));
+            Assert.That(retreivedProperties.ElementAt(0).Value, Is.EqualTo("value1"));
+            Assert.That(retreivedProperties.ElementAt(1).Key, Is.EqualTo("header2"));
+            Assert.That(retreivedProperties.ElementAt(1).Value, Is.EqualTo("value2"));
+            Assert.That(retreivedProperties.ElementAt(2).Key, Is.EqualTo("header3"));
+            Assert.That(retreivedProperties.ElementAt(2).Value, Is.EqualTo("value1,value2"));
+        }
+    }
+}
--- HelloTreacle/Requests/Properties/Retrieval/DefaultRequestPropertyRetriever.cs
+++ HelloTreacle/Requests/Properties/Retrieval/DefaultRequestPropertyRetriever.cs
@@ -1,5 +1,6 @@
-using System.Collections.Generic;
 using Microsoft.Owin;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HelloTreacle.Requests.Properties.Retrieval
 {
@@ -8,6 +9,14 @@ namespace HelloTreacle.Requests.Properties.Retrieval
         public IEnumerable<RequestProperty> Retrieve(IOwinRequest request)
         {
             var properties = new List<RequestProperty>();
+
+            var headerRequestProperties = request.Headers.Select(header => new RequestProperty
+            {
+                Key = header.Key,
+                Value = string.Join(",", header.Value.Select(v => v))
+            }).ToList();
+
+            properties.AddRange(headerRequestProperties);
 
             //todo: add many more
             properties.Add(new RequestProperty
